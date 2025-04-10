d4bcd1902b85bc8012c8f648e7926355010d9ee5
R4s0n3 <github@miomideal.com>
2025-04-10 14:19:54 +0200

    style changes

--- src/app/leaderboard/_components/mio-scores.tsx
+++ src/app/leaderboard/_components/mio-scores.tsx
@@ -6,7 +6,7 @@ export default function MioScoreList(){
     const [mioScores] = api.game.getMionaires.useSuspenseQuery()
     return <div className="size-full aspect-video flex flex-col items-center bg-gradient-to-br from-primary/90 to-primary-dark/30 border border-primary shadow-xl rounded-xl overflow-y-auto relative text-body">
         <h5 className="sticky top-0 p-2 px-4 w-full flex justify-center bg-primary-dark uppercase ">Recent Mionaires</h5>
-        {mioScores.length === 0 && <div className={`p-2 px-4 flex justify-between bg-primary-light`}>
+        {mioScores.length === 0 && <div className={`p-2 px-4 w-full flex justify-between bg-primary-light`}>
             <p className="text-center w-full">no scores..</p>
         </div>}
         {mioScores.map((u, i) => <div className={`p-2 px-4 text-sm w-full flex justify-between items-center bg-primary-dark/20 ${i % 2 && "bg-primary-light/20"}`} key={i}>
--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -18,17 +18,17 @@ export default async function Leaderboard() {
 
   return (
     <HydrateClient>
-      <main className="flex min-h-screen flex-col gap-8 p-4 items-center justify-center bg-gradient-to-b from-primary to-primary-dark text-body relative">
+      <main className="flex min-h-screen flex-col gap-8 p-4 items-center justify-center bg-gradient-to-b from-primary to-primary-dark text-body relative pt-8 pb-16">
       <Link href="/" className="absolute top-4 right-4">
       <HomeIcon className="size-8 text-body" />
       </Link>
       <h1 className="text-4xl uppercase font-black">Leaderboard</h1>
       <div className="w-full max-w-xl grid lg:grid-cols-[auto_1fr] gap-8">
 
-      {session?.user && <div className=" col-span-1 w-full flex justify-center items-center flex-col gap-4 uppercase aspect-video ">
+      {session?.user && <div className="col-span-1 w-full flex justify-center items-center flex-col gap-4 uppercase aspect-video ">
         <PlayerRank />
     </div>}
-    <div className="">
+    <div className="col-span-1">
       <MioScoreList />
     </div>
     <div className="w-full odd:col-span-full flex justify-center items-center flex-col gap-4 uppercase">
