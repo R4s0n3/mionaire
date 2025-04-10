dd82dc9f748324432b5f0306d84f045f4ed2743a
R4s0n3 <github@miomideal.com>
2025-04-10 13:15:47 +0200

    config stuff

--- src/env.js
+++ src/env.js
@@ -14,8 +14,6 @@ export const env = createEnv({
     AUTH_DISCORD_ID: z.string(),
     AUTH_DISCORD_SECRET: z.string(),
     DATABASE_URL: z.string().url(),
-    DEEPSEEK_API_KEY: z.string(),
-    OPENROUTER_API_KEY:z.string(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
@@ -36,11 +34,9 @@ export const env = createEnv({
    */
   runtimeEnv: {
     AUTH_SECRET: process.env.AUTH_SECRET,
-    DEEPSEEK_API_KEY: process.env.DEEPSEEK_API_KEY,
     AUTH_DISCORD_ID: process.env.AUTH_DISCORD_ID,
     AUTH_DISCORD_SECRET: process.env.AUTH_DISCORD_SECRET,
     DATABASE_URL: process.env.DATABASE_URL,
-    OPENROUTER_API_KEY: process.env.OPENROUTER_API_KEY,
     NEXT_PUBLIC_ADSENSE_CLIENT_ID:process.env.NEXT_PUBLIC_ADSENSE_CLIENT_ID,
     NODE_ENV: process.env.NODE_ENV,
   },
